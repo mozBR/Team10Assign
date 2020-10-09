632ff0151f21a7febb70c9bcece518737a0e03ae
wneves <wneves@byui.edu>
2020-10-09 10:28:17 -0600

    Initial commit

---
+++ src/assign04/Main.java
@@ -0,0 +1,9 @@
+package assign04;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, my name is Wanderson!");
+    }
+}

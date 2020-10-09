9f07de1079497c7189f286304789c0abb51d23f5
wneves <wneves@byui.edu>
2020-10-09 10:51:44 -0600

    change for stretch challenge

--- src/assign04/Main.java
+++ src/assign04/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Wanderson!");
+        System.out.println("This is a change for the stretch challenge");
     }
 }
